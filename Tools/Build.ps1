87e44eac854d7c30d4f9ba05e997b6b3694e29e3
Vogt-N <yaggoth@gmail.com>
2024-03-18 14:15:43 +0100

    Ajout d'une partie de la traduction des dialogues de Renées. Début traduction des dialogues de la radio. Ajout d'un script de build du fichier de traduction.

---
+++ Tools/Build.ps1
@@ -0,0 +1,38 @@
+## Chargement de 7zip
+pushd "H:\Program Files\7-Zip"
+$lo7zipFile = Get-Item .\7z.exe
+popd
+
+## Copie du script original
+if (!(Test-Path .\release\scripts.aod))
+{
+    Copy-item .\release\scripts_original_2.0.2.2.aod .\release\scripts.aod
+}
+
+## Mise à jour de l'archive
+## Ajout des fichiers dans la release
+## Premier paramètre : a = ajouter dans l'archive
+## Second paramètre  : emplacement de l'archive
+## 3ième paramètre : fichiers à traiter
+.$lo7zipFile.FullName u .\release\scripts.aod data\text\dialogues\english\*.xml
+
+
+<#
+[Reflection.assembly]::Load('System.IO.Compression')
+[Reflection.assembly]::Load('System.IO.Compression.FileStream')
+
+$loScriptsDeadState = [System.IO.Compression.ZipFile]::Open(".\Release\scripts.aod",[System.IO.Compression.ZipArchiveMode]::Update)
+$loGzipStream = [System.IO.Compression.GZipStream]::new()
+
+$loScriptsDeadState.Entries | Where-Object { 
+    $_.FullName -match '^data/text/dialogues/english/.+\.xml$' 
+} | ForEach-Object {
+    $_
+}
+
+Get-ChildItem .\data\text\dialogues\english | ForEach-Object {
+    $loScriptsDeadState.CreateEntry("$(data\text\dialogues\english\$_.Name)")
+}
+
+$loScriptsDeadState.dispose()
+#>
